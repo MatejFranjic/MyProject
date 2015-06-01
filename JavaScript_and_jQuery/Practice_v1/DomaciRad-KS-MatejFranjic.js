a507a00f5337d73b541d7647f02a6fd2676abb9b
MatejFranjic <franjic.matej93@gmail.com>
2015-06-01 16:30:15 +0200

    JS+jQuery
    JS and jQuery code

---
+++ JavaScript_and_jQuery/Practice_v1/DomaciRad-KS-MatejFranjic.js
@@ -0,0 +1,67 @@
+﻿function Osoba() {
+    this.ime = "";
+    this.prezime = "";
+    this.modelBicikla = "";
+    this.datumKupnje = "";
+    this.predenoKilometara = "";    
+}
+
+//provjeravam koliko je bicikl star
+Osoba.prototype.Starost = function () {
+    //splitam i okrećem
+    var datumKupljen = this.datumKupnje.split(".");
+    dkDan = Number(datumKupljen[0]);
+    dkMjesec = Number(datumKupljen[1]);
+    dkGodina = Number(datumKupljen[2]);
+    datumKupljen = Number(dkMjesec) + '.' + Number(dkDan) + '.' + Number(dkGodina);
+    datumKupljen = new Date(datumKupljen);
+
+    var danas = new Date();
+    danasDan = danas.getDate();
+    danasMjesec = danas.getMonth();
+    danasGodina = danas.getFullYear();
+    danas = Number(danasMjesec + 1) + '.' + Number(danasDan) + '.' + Number(danasGodina);
+    danas = new Date(danas);
+
+    var razlika = ((danas - datumKupljen) /1000 /60 /60 /24);
+    return Math.round(razlika);
+}
+
+Osoba.prototype.kilometraza = function () {    
+    var kilometraza = this.predenoKilometara / this.Starost();
+    return kilometraza.toFixed(2);
+}
+
+//formiram kako će to izgledati
+Osoba.prototype.toString = function () {    
+    return "Osoba: " + this.ime + ' ' + this.prezime +
+        "\nDatum kupnje bicikla: " + this.datumKupnje +
+        "\nPređeno kilometara: " + this.predenoKilometara + "km" +
+        "\nProsječno po danu: " + this.kilometraza()+"km" +
+        "\nStarost bicikla u danima: " + this.Starost()+
+        "\n----------------\n";
+}
+
+function upis() {
+    var a = new Osoba();
+    a.ime = prompt("Vaše ime:");
+    a.prezime = prompt("Vaše prezime:");
+    a.modelBicikla = prompt("Model bicikla:");
+    a.datumKupnje = prompt("Kojega datuma ste kupili bicikl:");
+    a.predenoKilometara = Number(prompt("Koliko ste kilometara prešli svojim biciklom:"));
+    return a;
+}
+
+//priprema polja
+var polje = [];
+
+do {
+    polje.push(upis());
+} while (confirm("Želite li unijeti još podataka?"));
+
+var ispis = "";
+for (var i = 0; i < polje.length; i++) {
+    ispis += polje[i].toString();
+}
+
+alert(ispis);
---
+++ JavaScript_and_jQuery/Practice_v2.2/DZ.js
@@ -0,0 +1,86 @@
+﻿//funkcija za random color zaglavlja (div-a)
+function slucajnaBoja() {
+    var red = Math.round(Math.random() * 256);
+    var green = Math.round(Math.random() * 256);
+    var blue = Math.round(Math.random() * 256);
+
+    return "#" + red.toString(16) + green.toString(16) + blue.toString(16); 
+}
+
+$(function () {
+    $("div > div:first-child").attr("id", "divZaglavlje");
+
+    $("ul > li:first-child > a").attr("id", "idOnama");
+    $("ul > li:nth-child(2) > a").attr("id", "Proizvodi");
+    $("ul > li:last-child > a").attr("id", "Kontakt");
+
+    $("#Proizvodi, #Kontakt").click(function (event) {
+        var naslovLinka = $(this).attr("id");
+        event.preventDefault();
+        $("#divZaglavlje").css({ "background-color": slucajnaBoja(), "color": "white" });
+        $('*').removeClass();
+        $(this).addClass("link");
+        $("h2").text(naslovLinka);
+
+        var pBroj = $("p").length;
+        if (naslovLinka == 'Proizvodi') {
+
+            for (var i = 0; i < pBroj; i++) {
+                if (i == 0) {
+                    var p1 = naslovLinka + " generički tekst paragrafa " + (i + 1);
+                }
+                else {
+                    var p2 = naslovLinka + " generički tekst paragrafa " + (i + 1);
+                }
+            }
+
+            $("p:first-of-type").text(p1);
+            $("p:nth-of-type(2)").text(p2);
+
+        } else {
+            for (var i = 0; i < pBroj; i++) {
+                if (i == 0) {
+                    var p1 = naslovLinka + " generički tekst paragrafa " + (i + 1);
+                }
+                else {
+                    var p2 = naslovLinka + " generički tekst paragrafa " + (i + 1);
+                }
+            }
+
+            $("p:first-of-type").html(p1);
+            $("p:nth-of-type(2)").text(p2);
+        }
+    });
+
+});
+
+
+window.onload = function() {
+    
+    var prviLink = document.getElementsByTagName('a')[0];
+
+    prviLink.onclick = function (e) {
+        e.preventDefault();
+        var atribut = document.getElementById("idOnama").getAttribute("id");
+
+        var divZaglavlje = document.getElementById("divZaglavlje");
+        divZaglavlje.style.color = "white";
+        divZaglavlje.style.backgroundColor = slucajnaBoja();
+
+        var h2 = document.getElementsByTagName("h2")[0];
+        h2.innerHTML = 'O nama';
+
+        var parBroj = document.getElementsByTagName("p").length;
+        for (var i = 0; i < parBroj; i++) {
+            document.getElementsByTagName("p")[i].innerText = h2.innerHTML + " generički tekst paragrafa " + (i + 1);
+        }
+
+        var brojLinkova = document.getElementsByTagName("a").length;
+        for (var i = 0; i < brojLinkova; i++) {
+            document.getElementsByTagName("a")[i].removeAttribute("class");
+        }
+
+        document.getElementById("idOnama").setAttribute("class", "link");
+    };
+}
+
